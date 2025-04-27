fb743b7806e5fd302b7e596b5896e73fd5d5f388
Backup158 <beekoop158@gmail.com>
2025-04-27 02:16:00 -0500

    feat: initialized shell script

---
+++ DarktideEnhancedDescriptionsUwUify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# In the working directory, run the UwUify script for Enhanced Descriptions
+# Assumes working directory is <Darktide Mods Folder>/Enhanced_descriptions
+# Assumes the UwUifier is in the working directory
+
+path_to_uwuifier_script='DarktideEnhancedDescriptionsUwUify.py'
